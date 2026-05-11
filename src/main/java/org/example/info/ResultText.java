a9e402c9ddd6feaeed4625ea7cab5163d5bd30bd
Thomas Alexandrescu <thomasalexandrescu@gmail.com>
2026-05-11 00:54:35 -0400

    Added ResultText

---
+++ src/main/java/org/example/info/ResultText.java
@@ -0,0 +1,51 @@
+package org.example.info;
+
+public class ResultText {
+    // WORK
+    public static final String WORK_1 = "";
+    public static final String WORK_2 = "";
+    public static final String WORK_3 = "";
+    public static final String WORK_4 = "";
+    public static final String WORK_5 = "";
+    public static final String WORK_6 = "";
+
+    // INVEST
+    public static final String INVEST_1 = "";
+    public static final String INVEST_2 = "";
+    public static final String INVEST_3 = "";
+    public static final String INVEST_4 = "";
+    public static final String INVEST_5 = "";
+    public static final String INVEST_6 = "";
+
+    // SHOP
+    public static final String SHOP_1 = "";
+    public static final String SHOP_2 = "";
+    public static final String SHOP_3 = "";
+    public static final String SHOP_4 = "";
+    public static final String SHOP_5 = "";
+    public static final String SHOP_6 = "";
+
+    // EAT
+    public static final String EAT_1 = "You decided to buy some coffee for 5$.\nIt turned out to not be very good and you need something else to eat.\n Surprisingly, 3/4 Americans drink coffee EVERY DAY. 36% are drinking 3 to 5 cups.\n Did you really need that coffee?\nHunger -5, Money -5$";
+    public static final String EAT_2 = "";
+    public static final String EAT_3 = "";
+    public static final String EAT_4 = "";
+    public static final String EAT_5 = "";
+    public static final String EAT_6 = "";
+
+    // BANK
+    public static final String BANK_1 = "";
+    public static final String BANK_2 = "";
+    public static final String BANK_3 = "";
+    public static final String BANK_4 = "";
+    public static final String BANK_5 = "";
+    public static final String BANK_6 = "";
+
+    // REST
+    public static final String REST_1 = "";
+    public static final String REST_2 = "";
+    public static final String REST_3 = "";
+    public static final String REST_4 = "";
+    public static final String REST_5 = "";
+    public static final String REST_6 = "";
+}
